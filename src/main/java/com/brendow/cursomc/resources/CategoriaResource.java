10df5c8b77674fe9b2c7f080688c69ffbc173362
Brendow <brendowcata1@gmail.com>
2021-07-08 14:05:53 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/brendow/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/brendow/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package com.brendow.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.brendow.cursomc.domain.Categoria;
+import com.brendow.cursomc.dto.CategoriaDTO;
 import com.brendow.cursomc.services.CategoriaService;
 
 @RestController
@@ -47,5 +50,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream()
+		.map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
